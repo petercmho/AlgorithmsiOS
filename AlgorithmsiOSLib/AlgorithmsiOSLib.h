ec5a91442ccb4e1da59dd7f8f9f7a607d9166c95
Peter Ho <peterho@petermacbookpro-7.local>
2016-12-13 20:12:52 +0800

    Initial Commit

---
+++ AlgorithmsiOSLib/AlgorithmsiOSLib.h
@@ -0,0 +1,19 @@
+//
+//  AlgorithmsiOSLib.h
+//  AlgorithmsiOSLib
+//
+//  Created by Peter Ho on 2016-12-13.
+//  Copyright © 2016 Peter Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlgorithmsiOSLib.
+FOUNDATION_EXPORT double AlgorithmsiOSLibVersionNumber;
+
+//! Project version string for AlgorithmsiOSLib.
+FOUNDATION_EXPORT const unsigned char AlgorithmsiOSLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlgorithmsiOSLib/PublicHeader.h>
+
+
