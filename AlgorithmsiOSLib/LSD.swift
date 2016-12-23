2044bea40c8ce391f90749fa5d771371158da662
Peter Ho <peterho@petermacbookpro-7.local>
2016-12-23 21:28:45 +0800

    Clean up.

--- AlgorithmsiOSLib/LSD.swift
+++ AlgorithmsiOSLib/LSD.swift
@@ -9,34 +9,26 @@
 import Foundation
 
 class LSD {
-    static func sort(_ a: [String], _ w: Int) {
-        let n = a.count
-        let r = 256     // extend ASCII alphabet size
-        var aux = [String]()
-        
-        aux.reserveCapacity(n)
-        
-    }
-    
-    static func indexCount(_ a: inout [String], _ w: Int) -> Void {
+    // LSD radix sort
+    static func indexCount(_ a: inout [String], _ w: Int) {
         let n = a.count
         let r = 256
         
         for index in 1...w {
-            var radix = Array<Int>(repeating: 0, count: 256 + 1)
+            var radix = Array<Int>(repeating: 0, count: r + 1)
             var aux = Array<String?>(repeating: nil, count: n)
             
-            // Count frequency of each index key
+            // Compute frequency counts
             for i in 0..<n {
                 let utf8Chars = a[i].utf8CString
                 let char = utf8Chars[w - index]
                 radix[Int(char) + 1] += 1
             }
-            // Accumulate each index key
+            // Compute cumulates
             for i in 1...r {
                 radix[i] += radix[i-1]
             }
-            // Move items based on key index
+            // Move data
             for i in 0..<n {
                 let char = a[i].utf8CString[w - index]
                 aux[radix[Int(char)]] = a[i]
