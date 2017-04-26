f1d3d808c121e86790a5f39cbcf8dfe4b963cd1d
Peter Ho <peterho@node-1w7jr9qq3bf4sfne2br9s5tb1.ipv6.telus.net>
2017-04-25 20:26:25 -0700

    Added skeleton MaxPQ class.

---
+++ AlgorithmsiOSLib/MaxPQ.swift
@@ -0,0 +1,19 @@
+//
+//  MaxPQ.swift
+//  AlgorithmsiOSLib
+//
+//  Created by Peter Ho on 2017-01-10.
+//  Copyright © 2017 Peter Ho. All rights reserved.
+//
+
+import Foundation
+
+class MaxPQ<Key>
+    where Key: Comparable {
+    func insert(v: Key) {
+        
+    }
+    func delMax() -> Key {
+        return Key()
+    }
+}
